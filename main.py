99efd3f9fe55ecb52c1067b3754795a16dcb4eb7
hkt2000 <hassantajudeen06@gmail.com>
2022-05-23 06:00:50 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,61 @@
+        # Addition and Subtration
+# example 1:
+a = 4
+b = 6
+c = -7
+sum = a + b + c
+print(sum)
+print(a+b)
+print(b-a)
+# example2
+a = 'Hassan'
+b = 'Kehinde'
+c = 'Tajudeen'
+print(a +' '+ b + ' '+ c)
+print(a,b,c)
+
+            # Multiplication
+a = 4
+b = 6
+multiply = a*b
+print(multiply)
+a = 'Hassan'
+b = 10*a
+print(b)
+                # Division
+a = 24
+b = 6
+divide = a/b
+c = int(divide)
+div=a//b
+print(divide)
+print(div)
+print(c)
+d = a%b
+e =a%5
+print(d)
+print(e)
+            # Exponential
+a=5
+b =3
+c= a**b
+print(c)
+a=100
+a=a+5
+print(a)
+a,b,c, = 2,3,"Hassan"
+d=a*b*c
+print(d)
+x=4
+x+=5
+print(x)
+
+# school = '''Name: Hassan
+# Class: Grade 6
+# Age: 9'''
+# print(school)
+# a = True
+# print(a)
+# name = input("Enter full name")
+# print('Full name:',name)
+
